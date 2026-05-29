3cb64961a7b8536292026c20f8544695142738ca
Souradeep Tarafdar <souradeep121@gmail.com>
2026-05-29 20:40:06 +0530

    Update adminlogin.js

--- frontend/js/adminlogin.js
+++ frontend/js/adminlogin.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:5000";
+const BASE_URL = "https://drive-legal.onrender.com/";
 
 document.getElementById("adminLoginForm").addEventListener("submit", async function(e) {
     e.preventDefault();
